8a32f53cdd849df82d7aab32e821d8276b863a60
DeonTama <JIKMbupPyV>
2023-03-17 15:30:24 +0700

    I just want to testing and now its fix

--- app/src/main/java/com/tsu/watertracker/MainActivity.kt
+++ app/src/main/java/com/tsu/watertracker/MainActivity.kt
@@ -14,18 +14,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var drink = 3000
+        var drunk = 0
+
         binding.button1.setOnClickListener{
-            var currentValue: Int = binding.drunkValue.text.toString().toInt()
-            binding.drunkValue.text = (currentValue + 250).toString()
-            when (binding.drunkValue.text = 3000){
+            drink -=250
+            drunk +=250
+
+            binding.drunkValue.text = "0"
+
+            var drunkingValue: Int = binding.drunkValue.text.toString().toInt()
+            binding.drunkValue.text = (drunk).toString()
+
+            var drinkingValue: Int = binding.waterValue.text.toString().toInt()
+            binding.waterValue.text = (drink).toString()
+
+            if (drink == 0){
                 val intent = Intent (this, SecondActivity::class.java)
                 startActivity(intent)
             }
-
-        }
-        binding.button1.setOnClickListener{
-            var currentValue: Int = binding.waterValue.text.toString().toInt()
-            binding.waterValue.text = (currentValue - 250).toString()
         }
     }
 }
